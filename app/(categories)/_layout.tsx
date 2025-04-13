006d313d19c14c23db801242936b160b039dce7d
Joel Võsu <joel@mindobambu.com>
2025-04-13 16:16:10 -0500

    Add routing for categories Home, Work and God and a profile page, that is not routed yet

---
+++ app/(categories)/_layout.tsx
@@ -0,0 +1,35 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {Tabs} from "expo-router";
+
+const _Layout = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Index',
+                    headerShown: false,
+                }} />
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                }} />
+            <Tabs.Screen
+                name="work"
+                options={{
+                    title: 'Work',
+                    headerShown: false,
+                }} />
+            <Tabs.Screen
+                name="god"
+                options={{
+                    title: 'God',
+                    headerShown: false,
+                }} />
+        </Tabs>
+    )
+}
+export default _Layout
---
+++ app/(categories)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-4xl text-teal-700 font-bold text-center mx-5">Laziness casts one into a deep sleep,
+          and an idle person will suffer hunger.</Text>
+      <Text className="text-2xl text-primary font-bold text-center">Proverbs 19:15</Text>
+    </View>
+  );
+}
